9b9aebf9bf1e61e39d4a0eb4a3fff6c990268b5d
Muhammad Ali <muhammad_ali@workmail.com>
2024-08-17 15:40:15 +0500

    Updated requirements.txt & Removed unused imports

--- bot.py
+++ bot.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
-import json
 import os
 import requests
 from bs4 import BeautifulSoup
